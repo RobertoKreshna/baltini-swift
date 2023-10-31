c5bff4958e34d1fa026b3bc60a005ae5282980d2
RobertoKreshna <roberto.kreshnain@gmail.com>
2023-10-31 17:32:05 +0700

    add change password

--- baltini-swift/Controllers/ChangePasswordViewController.swift
+++ baltini-swift/Controllers/ChangePasswordViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ChangePasswordViewController : UIViewController {
     var currentUser : User?
@@ -51,23 +52,17 @@ extension ChangePasswordViewController {
         pageStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
         pageStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
         
-        BackButton.addBackButton(to: pageStackView, title: "My Profile", sender: self)
+        BackButton.addBackButton(to: pageStackView, title: "Change Password", sender: self)
         pageStackView.setCustomSpacing(24, after: pageStackView.arrangedSubviews.last!)
-        CustomTextfield.addTextfield(to: pageStackView, placeholder: "First Name", isPassword: false, owner: self, text: currentUser?.firstName)
+        CustomTextfield.addTextfield(to: pageStackView, placeholder: "Old Password", isPassword: false, owner: self)
         pageStackView.setCustomSpacing(24, after: pageStackView.arrangedSubviews.last!)
-        CustomTextfield.addTextfield(to: pageStackView, placeholder: "Last Name", isPassword: false, owner: self, text: currentUser?.lastName)
+        CustomTextfield.addTextfield(to: pageStackView, placeholder: "New Password", isPassword: false, owner: self)
         pageStackView.setCustomSpacing(24, after: pageStackView.arrangedSubviews.last!)
-        CustomTextfield.addTextfield(to: pageStackView, placeholder: "Email", isPassword: false, owner: self, text: currentUser?.email)
+        CustomTextfield.addTextfield(to: pageStackView, placeholder: "Confirm New Password", isPassword: false, owner: self)
         pageStackView.setCustomSpacing(32, after: pageStackView.arrangedSubviews.last!)
         
-        let changePasswordButton = CustomButton.createUnderlinedButton(title: "CHANGE PASSWORD", action: UIAction(handler: { action in
-            print("change password")
-        }))
-        pageStackView.addArrangedSubview(changePasswordButton)
-        pageStackView.setCustomSpacing(40, after: pageStackView.arrangedSubviews.last!)
-        
-        let saveButton = CustomButton.createBlackButton(title: "SAVE", action: UIAction(handler: { action in
-            print("save")
+        let saveButton = CustomButton.createBlackButton(title: "CHANGE PASSWORD", action: UIAction(handler: { action in
+            self.changePasswordPressed(from: pageStackView)
         }))
         pageStackView.addArrangedSubview(saveButton)
         saveButton.widthAnchor.constraint(equalTo: pageStackView.widthAnchor).isActive = true
@@ -89,8 +84,82 @@ extension ChangePasswordViewController: UITextFieldDelegate {
 
 //MARK: Business Logic
 extension ChangePasswordViewController {
-    func goToChangePassword() {
+    func changePasswordPressed(from stack: UIStackView){
+        //get all textfield
+        let oldPasswordTextfield = stack.arrangedSubviews[2] as! UITextField
+        let newPasswordTextfield = stack.arrangedSubviews[5] as! UITextField
+        let confirmPasswordTextfield = stack.arrangedSubviews[8] as! UITextField
         
+        let oldPassword = oldPasswordTextfield.text!
+        let newPassword = newPasswordTextfield.text!
+        let confirmNewPassword = confirmPasswordTextfield.text!
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        if checkIsNotEmpty(oldPassword, newPassword, confirmNewPassword) {
+            if checkConfirm(newPassword, confirmNewPassword){
+                if checkOldPassword(inputted: oldPassword, real: getEditedAccountPassword(context)) {
+                    let editedAccount = getEditedAccount(context)
+                    editedAccount?.password = newPassword
+                    do{
+                        try context.save()
+                        CustomPopup.displayRegisterPopup(sender: self, title: "Account updated", toRoot: true)
+                    } catch {
+                        CustomToast.showErrorToast(msg: "Error updating account", sender: self)
+                    }
+                } else {
+                    CustomToast.showErrorToast(msg: "Old password is wrong", sender: self)
+                }
+            } else {
+                CustomToast.showErrorToast(msg: "Confirm password is wrong", sender: self)
+            }
+        } else {
+            CustomToast.showErrorToast(msg: "All fields required, please fill all the fields above", sender: self)
+        }
+    }
+    
+    func checkIsNotEmpty(_ oldPw: String, _ newPw: String, _ confirmPw: String) -> Bool {
+        return oldPw.isEmpty || newPw.isEmpty || confirmPw.isEmpty ? false : true
+    }
+    
+    func checkConfirm(_ pw: String, _ confirm: String) -> Bool {
+        return pw == confirm
+    }
+    
+    func checkOldPassword(inputted: String, real: String) -> Bool {
+        return inputted == real
+    }
+    
+    func getEditedAccountPassword(_ context: NSManagedObjectContext) -> String {
+        var res: User? = nil
+        
+        let request: NSFetchRequest<User> = User.fetchRequest()
+        do{
+            let allUser = (try context.fetch(request))
+            allUser.forEach { user in
+                if(user.id == currentUser?.id) { res = user }
+            }
+        } catch {
+            CustomToast.showErrorToast(msg: "No address found", sender: self)
+        }
+        
+        return res?.password ?? ""
+    }
+    
+    func getEditedAccount(_ context: NSManagedObjectContext) -> User? {
+        var res: User? = nil
+        
+        let request: NSFetchRequest<User> = User.fetchRequest()
+        do{
+            let allUser = (try context.fetch(request))
+            allUser.forEach { user in
+                if(user.id == currentUser?.id) { res = user }
+            }
+        } catch {
+            CustomToast.showErrorToast(msg: "No address found", sender: self)
+        }
+        
+        return res
     }
 }
 
