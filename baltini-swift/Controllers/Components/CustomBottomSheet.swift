58fd2dddaeda45ad43a84f1a0e7c3f7d9d32c281
RobertoKreshna <roberto.kreshnain@gmail.com>
2023-12-04 15:28:30 +0700

    price filter to custom bot sheet

--- baltini-swift/Controllers/Components/CustomBottomSheet.swift
+++ baltini-swift/Controllers/Components/CustomBottomSheet.swift
@@ -246,4 +246,128 @@ class CustomBottomSheet {
         return contentView
     }
     
+    static func createPriceFilterContent(owner: RangeSeekSliderDelegate, minLabel: UILabel, maxLabel: UILabel, close: @escaping () -> Void) -> UIView {
+        let contentView = UIStackView()
+        contentView.axis = .vertical
+        contentView.alignment = .center
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.backgroundColor = .white
+        contentView.layer.cornerRadius = 16
+        
+        let indicator = createGrayIndicator()
+        let backButton = BackButton.createBackButton(title: "FILTER - PRICE", icName: "icBack", usePadding: false, tapped: UIAction(handler: { action in
+            close()
+        }))
+        //row
+        let minStack = createPriceStack(priceLabel: minLabel)
+        let maxStack = createPriceStack(priceLabel: maxLabel)
+        
+        let tillLabel = UILabel()
+        tillLabel.text = "-"
+        tillLabel.font = UIFont(name: "Futura-Medium", size: 16)
+        tillLabel.textColor = .black
+        tillLabel.textAlignment = .center
+        tillLabel.setContentHuggingPriority(.required, for: .horizontal)
+        
+        let row = UIStackView()
+        row.translatesAutoresizingMaskIntoConstraints = false
+        row.axis = .horizontal
+        row.spacing = 16
+        row.addArrangedSubview(minStack)
+        row.addArrangedSubview(tillLabel)
+        row.addArrangedSubview(maxStack)
+        
+        minStack.widthAnchor.constraint(equalTo: maxStack.widthAnchor).isActive = true
+        
+        //slider
+        let cgFloatMin = CGFloat((minLabel.text! as NSString).doubleValue)
+        let cgFloatMax = CGFloat((maxLabel.text! as NSString).doubleValue)
+        let slider = createSlider(min: cgFloatMin, max: cgFloatMax, minLabel: minLabel, maxLabel: maxLabel, owner: owner)
+        //button
+        let filterButton = CustomButton.createBlackButton(title: "FILTER", action: UIAction(handler: { action in
+            SortFilterValue.shared.addFilterTo(key: "Price", value: [minLabel.text!, maxLabel.text!], replace: true)
+            close()
+        }))
+        
+        contentView.addArrangedSubview(indicator)
+        contentView.setCustomSpacing(12, after: indicator)
+        contentView.addArrangedSubview(backButton)
+        contentView.setCustomSpacing(34, after: backButton)
+        contentView.addArrangedSubview(row)
+        contentView.setCustomSpacing(37, after: row)
+        contentView.addArrangedSubview(slider)
+        contentView.setCustomSpacing(40, after: slider)
+        contentView.addArrangedSubview(filterButton)
+        
+        indicator.widthAnchor.constraint(equalToConstant: 35).isActive = true
+        indicator.heightAnchor.constraint(equalToConstant: 4).isActive = true
+        backButton.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        backButton.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+        row.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        row.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+        slider.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        slider.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+        filterButton.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        filterButton.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+        
+        return contentView
+    }
+    
+    static private func createSlider(min: CGFloat, max: CGFloat, minLabel: UILabel, maxLabel: UILabel, owner: RangeSeekSliderDelegate) -> RangeSeekSlider {
+        let slider = RangeSeekSlider()
+        //min max
+        slider.minValue = 72
+        slider.selectedMinValue = min
+        slider.maxValue = 2096
+        slider.selectedMaxValue = max
+        slider.minDistance = 50
+        slider.step = 1
+        //label
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = "$"
+        slider.numberFormatter = formatter
+        slider.minLabelFont = UIFont(name: "Futura-Medium", size: 14)!
+        slider.minLabelColor = .black
+        slider.maxLabelFont = UIFont(name: "Futura-Medium", size: 14)!
+        slider.maxLabelColor = .black
+        //ui
+        slider.handleImage = UIImage(named: "icSlider")
+        slider.tintColor = .black.withAlphaComponent(0.5)
+        slider.colorBetweenHandles = .black
+        slider.delegate = owner
+        
+        return slider
+    }
+    
+    static private func createPriceStack(priceLabel: UILabel) -> UIStackView {
+        let column = UIStackView()
+        column.translatesAutoresizingMaskIntoConstraints = false
+        column.axis = .vertical
+        
+        let row = UIStackView()
+        row.translatesAutoresizingMaskIntoConstraints = false
+        row.axis = .horizontal
+        
+        let currencyLabel = UILabel()
+        currencyLabel.text = "$"
+        currencyLabel.font = UIFont(name: "Futura-Medium", size: 16)
+        currencyLabel.textColor = .black
+        currencyLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        
+        row.addArrangedSubview(currencyLabel)
+        row.addArrangedSubview(priceLabel)
+        
+        let border = CustomSeparator.createHorizontalLine(width: 1, color: .brandGray)
+        
+        column.addArrangedSubview(row)
+        column.setCustomSpacing(4, after: row)
+        column.addArrangedSubview(border)
+        
+        border.leftAnchor.constraint(equalTo: column.leftAnchor).isActive = true
+        border.rightAnchor.constraint(equalTo: column.rightAnchor).isActive = true
+        
+        return column
+    }
+    
 }
